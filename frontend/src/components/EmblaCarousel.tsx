f09e729ef566653a1f3516d3856486a22c5a840d
Chernovolos <mariachernovolos@gmail.com>
2026-04-27 17:46:48 +0300

    Implement product details logic with size/color selection and cart.
    Added:
    - memoized stock availability logic
    - size and color selection with availability checks
    - auto-switching for unavailable combinations
    - add to cart functionality
    - loading and out-of-stock states
    - UI state handling (active/disabled)

--- frontend/src/components/EmblaCarousel.tsx
+++ frontend/src/components/EmblaCarousel.tsx
@@ -1,8 +1,14 @@
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useState, useCallback } from "react";
 
-const images = [1, 2, 3];
-const EmblaCarousel = () => {
+// const images = [1, 2, 3];
+type Props = {
+  images: string[];
+  outOfStock: boolean;
+};
+
+const EmblaCarousel = ({images = [], outOfStock}: Props) => {
+  if (!images.length) return null;
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel();
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
     axis: "y",
@@ -11,13 +17,14 @@ const EmblaCarousel = () => {
   });
 
   const [selectedIndex, setSelectedIndex] = useState(0);
+  console.log("outOfStock", outOfStock)
 
   const onThumbClick = useCallback(
     (index: number) => {
       if (!emblaMainApi || !emblaThumbsApi) return
       emblaMainApi.scrollTo(index)
     },
-    [emblaMainApi, emblaThumbsApi]
+    [emblaMainApi, emblaThumbsApi],
   )
 
   const onSelect = useCallback(() => {
@@ -51,36 +58,49 @@ const EmblaCarousel = () => {
   return (
     <div className="flex gap-4 max-w-3x">
 
-      {/*THUMBS LEFT*/}
-      <div className="overflow-hidden h-130" ref={emblaThumbsRef}>
+      {/*THUMBS LEFT*/ }
+      <div className="overflow-hidden h-130" ref={ emblaThumbsRef }>
         <div className="flex flex-col gap-4">
-          {images.map((num, i) => (
+          { images.map((img, i) => (
             <div
-              key={i}
-              onClick={() => onThumbClick(i)}
-              className={`cursor-pointer h-20 w-20 border-2 flex items-center justify-center text-xl font-bold ${
-                i === selectedIndex
-                  ? "border-blue-500"
-                  : "border-transparent"
-              }`}
+              key={ i }
+              onClick={ () => onThumbClick(i) }
+              className={ `relative cursor-pointer h-20 w-20 border-2 flex items-center justify-center ${
+                i === selectedIndex ? "border-gray-700" : "border-transparent"
+              } ${ outOfStock ? "opacity-45" : "" }` }
             >
-              {num}
+              <img src={ img } alt={ `thumb-${ i }` } className="object-cover w-full h-full"/>
+              { outOfStock && (
+                <div className="absolute flex items-center justify-center">
+                  <span className="out-of-stock-thumb-left-title">
+                    Out of stock
+                  </span>
+                </div>
+              ) }
             </div>
-          ))}
+          )) }
         </div>
       </div>
 
-       {/*MAIN */}
-      <div className="overflow-hidden flex-1 px-6" ref={emblaMainRef}>
+      {/*MAIN */ }
+      <div className="overflow-hidden flex-1" ref={ emblaMainRef }>
         <div className="flex">
-          {images.map((num, i) => (
+          { images.map((img, i) => (
             <div
-              key={i}
-              className="min-w-full h-130 flex items-center justify-center text-4xl font-bold bg-gray-200"
+              key={ i }
+              className={ `relative min-w-full h-130 flex items-center justify-center text-4xl font-bold bg-gray-200
+                ${ outOfStock ? "opacity-45" : "" } ` }
             >
-              {num}
+              <img src={ img } alt={ `thumb-${ i }` } className="object-cover w-full h-full"/>
+              { outOfStock && (
+                <div className="absolute flex items-center justify-center">
+                  <span className="out-of-stock-main-title">
+                    Out of stock
+                  </span>
+                </div>
+              ) }
             </div>
-          ))}
+          )) }
         </div>
       </div>
 
--- frontend/src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ frontend/src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,83 +1,217 @@
+import { useMemo, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import EmblaCarousel from "@/components/EmblaCarousel.tsx";
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
-import { selectProductDetails } from "@/pages/ProductDetailsPage/productDetailsSlice.ts";
-import { useEffect } from "react";
+import { selectProductDetails, selectProductLoading } from "@/pages/ProductDetailsPage/productDetailsSlice.ts";
 import { fetchProductById } from "@/pages/ProductDetailsPage/productDetailsAPI.ts";
-import type {  Gender } from "../../types/Category.ts";
+import type { Gender } from "../../types/Category.ts";
 import type { Color } from "@/types/Color.ts";
 import type { Size } from "@/types/Size.ts";
 
-
 const ProductDetailsPage = () => {
-  const { category, cartId} = useParams<{
+  const {category, cartId} = useParams<{
     category: Gender;
     cartId: string;
   }>();
   const dispatch = useAppDispatch();
   const product = useAppSelector(selectProductDetails);
+  const loading = useAppSelector(selectProductLoading);
+
+  const [selectedSize, setSelectedSize] = useState<Size | null>(null);
+  const [selectedColor, setSelectedColor] = useState<Color | null>(null);
+  const [cart, setCart] = useState<ProductCart[]>([]);
 
   useEffect(() => {
     if (!category || !cartId) return;
-    dispatch(fetchProductById({ gender: category , id: cartId }));
-
+    dispatch(fetchProductById({gender: category, id: cartId}));
   }, [category, cartId, dispatch])
 
-  const stockAvailabilityMap: Map<string, number> = new Map();
-  const availableColors: Set<Color> = new Set();
-  const availableSizes: Set<Size> = new Set();
-
-  if (product !== null) {
-    for (let stock of product.stock) {
-      // get all unique colors
-      if(!availableColors.has(stock.color)) {
-        availableColors.add(stock.color);
-      }
-      // get all unique sizes
-      if(!availableSizes.has(stock.size)) {
-        availableSizes.add(stock.size);
-      }
-
-      // put to stockAvailabilityMap
-      stockAvailabilityMap.set(`${stock.color.code}_${stock.size}`, stock.available);
+  const {stockAvailabilityMap, availableColors, availableSizes} = useMemo(() => {
+    if (!product) {
+      return {
+        stockAvailabilityMap: new Map<string, number>(),
+        availableColors: [] as Color[],
+        availableSizes: [] as Size[],
+      };
     }
+
+    const map = new Map<string, number>();
+    const colorsMap = new Map<number, Color>();
+    const sizesSet = new Set<Size>();
+
+    for (const stock of product.stock) {
+      sizesSet.add(stock.size);
+      colorsMap.set(stock.color.id, stock.color);
+
+      map.set(`${ stock.color.code }_${ stock.size }`, stock.available);
+    }
+
+    return {
+      stockAvailabilityMap: map,
+      availableColors: Array.from(colorsMap.values()),
+      availableSizes: Array.from(sizesSet.values()),
+    };
+  }, [product]);
+
+  useEffect(() => {
+    if (!product) return;
+
+    const firstAvailable = product.stock.find((s) => s.available > 0);
+    if (firstAvailable) {
+      setSelectedSize(firstAvailable.size);
+      setSelectedColor(firstAvailable.color);
+    } else {
+      setSelectedSize(null);
+      setSelectedColor(null);
+    }
+
+  }, [product])
+
+  if (loading) return <div>Loading...</div>;
+
+  if (!product) return <div>No product found</div>;
+
+  const {title, images, price, description, subcategory} = product;
+
+  const getQuantity = (color?: Color | null, size?: Size | null) => {
+    if (!color || !size) return 0;
+    console.log(stockAvailabilityMap.get(`${ color.code }_${ size }`) ?? 0)
+    return stockAvailabilityMap.get(`${ color.code }_${ size }`) ?? 0;
+  };
+
+  const isSizeAvailable = (size: Size) => {
+    if (!selectedColor) {
+      return availableColors.some((color) => getQuantity(color, size) > 0);
+    }
+    return getQuantity(selectedColor, size) > 0;
+  };
+
+  const isColorAvailable = (color: Color) => {
+    if (!selectedSize) {
+      return availableSizes.some((size) => getQuantity(color, size) > 0);
+    }
+    return getQuantity(color, selectedSize) > 0;
+  };
+
+  const handleSizeChange = (size: Size) => {
+    setSelectedSize(size);
+
+    if (selectedColor && getQuantity(selectedColor, size) === 0) {
+      const newColor = availableColors.find(
+        (c) => getQuantity(c, size) > 0,
+      );
+      setSelectedColor(newColor ?? null);
+    }
+  };
+
+  const handleColorChange = (color: Color) => {
+    setSelectedColor(color);
+
+    if (selectedSize && getQuantity(color, selectedSize) === 0) {
+      const newSize = availableSizes.find(
+        (s) => getQuantity(color, s) > 0,
+      );
+      setSelectedSize(newSize ?? null);
+    }
+  };
+
+  type ProductCart = {
+    id: number
+    quantity: number;
+    color: Color;
+    size: Size;
   }
-  console.log("availableColors", availableColors);
-  console.log("availableSizes", availableSizes);
-  console.log("stockAvailabilityMap", stockAvailabilityMap);
+  const addToCart = (newItem: ProductCart, products: ProductCart[])
+    : ProductCart[] => {
+    const existingItem = products.find((item) =>
+      item.id === newItem.id && item?.color.id === newItem?.color.id && item.size === newItem.size,
+    );
 
+    if (existingItem) {
+      return products.map((item) => (
+        item === existingItem ?
+          {...item, quantity: item.quantity + newItem.quantity} : item
+      ))
+    }
+    return [...products, newItem];
+  }
 
+  const addItemToCart = () => {
+    if (!selectedColor || !selectedSize) return;
 
-  console.log("product", product);
+    const newItem: ProductCart = {
+      id: product.id,
+      quantity: 1,
+      color: selectedColor,
+      size: selectedSize,
+    };
 
-  return (
+    setCart((prev) => addToCart(newItem, prev));
+  };
+
+  const currentQuantity = getQuantity(selectedColor, selectedSize);
+  const outOfStock = product.stock.every((s) => s.available === 0);
+
+  return loading ? (<div className="container">Loading...</div>) : (
     <section className="section section-product-details">
       <div className="container">
-        {/*<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-12 gap-y-20">*/}
-        <div className="grid grid-cols-12 gap-20">
-          <div className="col-span-7">
-            <EmblaCarousel/>
+        <div className="grid grid-cols-12 gap-x-20">
+          <div className="col-span-8">
+            <EmblaCarousel images={ images ?? [] } outOfStock={ outOfStock }/>
           </div>
-          <div className="col-span-5 flex flex-col">
-            <h2 className="product-title">Apollo</h2>
-            <p className="product-subtitle">Sub category</p>
+          <div className="col-span-4 flex flex-col">
+            <h2 className="product-title">{ title }</h2>
+            <p className="product-subtitle">{ subcategory.subcategory_name }</p>
 
             <p className="product-size">size:</p>
             <div className="product-btn-wrapper">
-              {[...availableSizes].map((size) => {
+              { outOfStock ? <div>OUT OF STOCK</div> : availableSizes.map((size) => {
                 return (
-                  <button key={size} className="btn btn-size">{size}</button>
+                  <button
+                    key={ size }
+                    className={ `btn btn-size ${
+                      selectedSize === size ? "activeSize" : ""
+                    } ${ isSizeAvailable(size) ? "" : "disabled:opacity-40 not-available-size" }` }
+                    onClick={ () => handleSizeChange(size) }
+                    // disabled={!isSizeAvailable(size)}
+                  >{ size }</button>
                 )
-              })}
+              }) }
             </div>
 
             <p className="product-color">color:</p>
+            <div className="product-btn-wrapper">
+              { availableColors.map((color) => {
+                return (
+                  <button
+                    key={ color.id }
+                    className={ `btn btn-color ${
+                      selectedColor?.id === color.id ? "activeColor" : ""
+                    } ${ isColorAvailable(color) ? "" : "disabled:opacity-40 not-available-color" }` }
+                    style={ {backgroundColor: color.hex} }
+                    onClick={ () => handleColorChange(color) }
+                    disabled={ !isColorAvailable(color) }
+                  >
+                  </button>
+                )
+              }) }
+            </div>
+
+            <p className="product-price-title">price:</p>
+            <p className="product-price">${ price }</p>
+            <button
+              disabled={ !selectedColor || !selectedSize || !currentQuantity }
+              onClick={ addItemToCart }
+              className={ `btn ${ (!selectedColor || !selectedSize || !currentQuantity) ? "disabled" : "" }` }>add to
+              cart
+            </button>
+            <p className="product-description">
+              { description }
+            </p>
           </div>
         </div>
-
       </div>
     </section>
-
   )
 }
 
